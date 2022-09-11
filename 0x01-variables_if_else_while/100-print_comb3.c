8c279c9f40aa9e38c97d3c223b62a235d0535be7
alexkiiru <alexkiiru9@gmail.com>
2022-09-11 01:32:13 -0700

    print_comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,15 +9,15 @@ int main(void)
 {
 	int n, m;
 
-	for(n = 48; n <= 56; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for(m = 49; m <= 57; m++)
+		for (m = 49; m <= 57; m++)
 		{
-			if(m > n)
+			if (m > n)
 			{
 				putchar(n);
 				putchar(m);
-				if(n !=56 || m !=57)
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
@@ -28,5 +28,3 @@ int main(void)
 	putchar('\n');
 	return (0);
 }
-			
-
