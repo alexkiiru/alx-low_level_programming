6df2684d3b98cec06107419e99110cb564efd4a1
alexkiiru <alexkiiru9@gmail.com>
2022-09-21 13:25:59 -0700

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _strcmp - function which compares two strings and
+ * _strcmp - function which compare two strings and
  * @s1: first string
  * @s2: second string
- * Return: returns zero if s1 == s2
+ * Return: rturns zero if s1 == s2
  * returns negative number if s1 < s2
  * returns positive number if s1 > s2
  */
