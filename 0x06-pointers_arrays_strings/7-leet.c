c993dd7812b698a1356294641d7674dfe23dfbbc
alexkiiru <alexkiiru9@gmail.com>
2022-09-21 14:22:42 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * leet - leet encoder
+ * @str: string to be encoded
+ *
+ * Return: address of the encoded string
+ */
+
+char *leet(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		str[i] = transform(str[i]);
+		i++;
+	}
+	return (str);
+}
+
+/**
+ * transform - helper function to map a letter with its leet enconding
+ * @x: char to be encoded
+ * Return: the encoded char
+ */
+
+char transform(char x)
+{
+	char mapping_low[8] = {'o', 'l', '\0', 'e', 'n', '\0', 't'};
+	char mapping_upper[8] = {'O', 'L', '\0', 'E', 'N', '\0', 'T'};
+	int i = 0;
+	char replacement = x;
+
+	while (i < 8)
+	{
+		if (x == mapping_low[i] || X == mapping_upper[i])
+		{
+			replacement = i + 0;
+			break;
+		}
+		i++;
+	}
+	return (replacement);
+}
