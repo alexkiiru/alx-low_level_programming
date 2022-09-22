568823474384475cad49c976c40595c4d8c873db
alexkiiru <alexkiiru9@gmail.com>
2022-09-22 07:00:33 -0700

    len

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ *_strlen - returns the length of a string
+ *@s: string
+ *Return: returns length as integer;
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * cap_string - capitalizes all words in a string
  * @s: string to capitalize
  * Return: address of s
