5cc726a6070015df85e06a9e9b5ec1f796452672
alexkiiru <alexkiiru9@gmail.com>
2022-10-07 12:18:16 -0700

    calloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -30,7 +30,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (j > n)
 		j = n;
-	s = malloc(sizeof(char) * (i + j + 1));
+	s = malloc(sizeof(char) * (i + j + l));
 	if (s == NULL)
 		return (NULL);
 	for (k = 0; k < i; k++)
